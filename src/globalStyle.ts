c7970ef2e01f36c379895bd2252237ae065520fe
Breno <eusoubrenovieira@gmail.com>
2022-08-19 13:07:16 -0300

    found a issue

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -8,9 +8,6 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     outline: 0;
   }
-  html, body, #root {
-    height: 100%;
-  }
   body {
     text-rendering: optimizeLegibility !important;
     -webkit-font-smoothing: antialiased !important;
